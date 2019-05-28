03ca76d0347db27fb722a137cc48b09cd545e37c
Randhall Rangel <rrangel@est.frba.utn.edu.ar>
2019-05-27 22:30:06 -0300

    Inclusion de FireBase

---
+++ src/config/db_config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAvyCuPCSabvoZGRlGFB-S8Y4nqKpiDv0k",
+    authDomain: "db-config-rd.firebaseapp.com",
+    databaseURL: "https://db-config-rd.firebaseio.com",
+    projectId: "db-config-rd",
+    storageBucket: "db-config-rd.appspot.com",
+    messagingSenderId: "242867612369",
+    appId: "1:242867612369:web:8c5b4c3ac7df7432"
+  };
